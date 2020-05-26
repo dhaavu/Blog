226ba0726f26a5099e12c639317eeb520f204f14
dhaval_temp@yahoo.in <dhaval_temp@yahoo.in>
2020-05-26 13:01:27 -0400

    add  To server

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ var passportLocalMongoose  = require("passport-local-mongoose");
 var bodyParser = require('body-parser');
 var rellax = require("rellax"); 
 var flash = require("connect-flash"); 
+var async = require("async"); 
+var nodemailer = require("nodemailer"); 
+var crypto = require("crypto"); 
+
 
 //mongodb://localhost/cxamplified
-mongoose.connect("mongodb+srv://dhaval:Welcome30@cluster0-71yob.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true } ,  function (err){
+mongoose.connect("mongodb+srv://dhaval:Welcome30@cluster0-71yob.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true , autoIndex: true,} ,  function (err){
     if (err)
     console.log("error changed  connecting to database  ", err);
     else
@@ -49,9 +53,10 @@ app.get('/register', function(req, res){
 
 app.post('/register', function(req, res){
     console.log(req.body.username + "/" + req.body.password);
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    User.register(new User({username: req.body.username, firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email}), req.body.password, function(err, user){
         if(err){
             console.log("error registering the user: " + err);
+            req.flash("error", "error registering the user: " + err ); 
             return res.render('./register');
         }
         else {
@@ -94,6 +99,68 @@ function isLoggedIn(req, res, next ){
     res.redirect("/login");
 
 }
+
+app.post('/forgot', function(req, res, next) {
+    async.waterfall([
+      function(done) {
+        crypto.randomBytes(20, function(err, buf) {
+          var token = buf.toString('hex');
+          done(err, token);
+        });
+      },
+      function(token, done) {
+        User.findOne({ email: req.body.email }, function(err, user) {
+          if (!user) {
+            req.flash('error', 'No account with that email address exists.');
+            return res.redirect('/forgot');
+          }
+  
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+  
+          user.save(function(err) {
+            done(err, token, user);
+          });
+        });
+      },
+      function(token, user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: 'Gmail', 
+          auth: {
+            user: 'learntocodeinfo@gmail.com',
+            pass: process.env.GMAILPW
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: 'learntocodeinfo@gmail.com',
+          subject: 'Node.js Password Reset',
+          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          console.log('mail sent');
+          req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+          done(err, 'done');
+        });
+      }
+    ], function(err) {
+      if (err) return next(err);
+      res.redirect('/forgot');
+    });
+  });
+  
+
+
+
+
+
+/**********************
+    Main routes 
+***********************/ 
+
 app.get('/', function (req, res){
 
     res.render('home');
